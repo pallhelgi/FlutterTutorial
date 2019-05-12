56f9c530d139e7947bfba0204b9f768c12086c86
pallhelgi <pallhelgi@gmail.com>
2019-05-12 15:37:01 +0000

    image and text child widgets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   //flutter reads from main function in main.dart to start the app
   // runApp is imported from material.dart, it takes widget as an argument
   // We create MyApp class widget which then draws the app to the screen
   runApp(MyApp());
 }
 
+// void main () => runApp(MyApp()); Is also allowed if the statement is short
+
 class MyApp extends StatelessWidget {
   // This is the top most base widget
   // MyApp extends StatelessWidget from the flutter package
   // Because of that build function needs to be implemented
   // It tells you how the base should appear
-  build(context) {
+  // Stateless widget requires us to overrrite the build method
+  @override // added later
+  Widget build(BuildContext context) {
+    // Widget and BuildContex added so they are no longer infered, it's better
     // Build return MaterialApp which is the start for all apps
+    // build must always return a widget
     // We can use it to create something on the screen.
     // is a wrapper for the entire app
     // we can tell it what to draw on the screen
@@ -25,6 +31,14 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
+        ),
+        body: Card(
+          child: Column(
+            children: <Widget>[
+              Image.asset('assets/food.jpg'),
+              Text('Food Paradise')
+            ],
+          ),
         ),
       ),
     );
